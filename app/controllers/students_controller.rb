69e7dab680eebcf4ef3d28832b4d9a76501f60bd
Riley Moore <rilemoore@gmail.com>
2019-12-13 23:21:31 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,7 @@ class StudentsController < ApplicationController
       @student.active = true
     end
     @student.save
-    redirect_to student_path(:show)
+    redirect_to student_path(@student)
   end
 
   private
